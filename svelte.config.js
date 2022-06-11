01c1c6192f7561841df09f15301a626fd8c24dbb
kj <komacine@protonmail.com>
2022-06-11 16:06:53 +0100

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,19 @@
+import adapter from '@sveltejs/adapter-cloudflare';
+import path from 'path';
+import preprocess from 'svelte-preprocess';
+
+let config = {
+	preprocess: preprocess(),
+	kit: {
+		adapter: adapter(),
+		vite: {
+			resolve: {
+				alias: {
+					$root: path.resolve('./src')
+				}
+			},
+		},
+	}
+};
+
+export default config;
