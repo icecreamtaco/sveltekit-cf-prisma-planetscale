ec2124b4a20426bd36af5c3bda4f97defa8064a2
kj <komacine@protonmail.com>
2022-06-12 17:43:26 +0100

    undo this

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,9 +1,3 @@
 import { PrismaClient } from '@prisma/client/edge';
 
-import { browser } from '$app/env';
-
-if (!browser) {
-	db = new PrismaClient();
-}
-
-export let db;
+export const db = new PrismaClient();
