106ba5ce1a7ac504b6f2f2166aa2fab63c09b2a4
kj <komacine@protonmail.com>
2022-06-13 15:51:51 +0100

    more detailed err

--- src/routes/auth/login/index.js
+++ src/routes/auth/login/index.js
@@ -19,103 +19,112 @@ export const post = async ({ request }) => {
 	const isVerifiedMessage = verifyMessageSignature({ message, signature });
 
 	if (isVerifiedMessage) {
-		// generate access and refresh tokens
+		try {
+			// generate access and refresh tokens
 
-		let userId;
-		let accessToken;
-		let refreshToken;
-		let refreshTokenHash;
+			let userId;
+			let accessToken;
+			let refreshToken;
+			let refreshTokenHash;
 
-		let user = await db.user.findUnique({
-			where: {
-				stxAddress
-			}
-		});
-
-		if (user) {
-			// update or create new session token
-			userId = user.id;
-			accessToken = await jwt.sign(
-				{ userId, exp: Math.floor(Date.now() / 1000) + constants.ACCESS_TOKEN_EXPIRE_TIME },
-				import.meta.env.VITE_ACCESS_TOKEN_SECRET
-			);
-			refreshToken = await jwt.sign(
-				{ userId, exp: Math.floor(Date.now() / 1000) + constants.REFRESH_TOKEN_EXPIRE_TIME },
-				import.meta.env.VITE_REFRESH_TOKEN_SECRET
-			);
-			refreshTokenHash = crypto.createHash('sha256').update(refreshToken, 'utf8').digest('hex');
-
-			user = await db.session.upsert({
+			let user = await db.user.findUnique({
 				where: {
-					userId_device: {
-						userId,
-						device
-					}
-				},
-				update: {
-					refreshTokenHash
-				},
-				create: {
-					userId,
-					device,
-					refreshTokenHash
-				}
-			});
-		} else {
-			userId = uuidv4();
-
-			accessToken = await jwt.sign(
-				{ userId, exp: Math.floor(Date.now() / 1000) + constants.ACCESS_TOKEN_EXPIRE_TIME },
-				import.meta.env.VITE_ACCESS_TOKEN_SECRET
-			);
-			refreshToken = await jwt.sign(
-				{ userId, exp: Math.floor(Date.now() / 1000) + constants.REFRESH_TOKEN_EXPIRE_TIME },
-				import.meta.env.VITE_REFRESH_TOKEN_SECRET
-			);
-			refreshTokenHash = crypto.createHash('sha256').update(refreshToken, 'utf8').digest('hex');
-
-			const createUser = db.user.create({
-				data: {
-					id: userId,
 					stxAddress
 				}
 			});
 
-			const createSession = db.session.create({
-				data: {
-					userId,
-					refreshTokenHash,
-					device
-				}
-			});
+			if (user) {
+				// update or create new session token
+				userId = user.id;
+				accessToken = await jwt.sign(
+					{ userId, exp: Math.floor(Date.now() / 1000) + constants.ACCESS_TOKEN_EXPIRE_TIME },
+					import.meta.env.VITE_ACCESS_TOKEN_SECRET
+				);
+				refreshToken = await jwt.sign(
+					{ userId, exp: Math.floor(Date.now() / 1000) + constants.REFRESH_TOKEN_EXPIRE_TIME },
+					import.meta.env.VITE_REFRESH_TOKEN_SECRET
+				);
+				refreshTokenHash = crypto.createHash('sha256').update(refreshToken, 'utf8').digest('hex');
 
-			user = await db.$transaction([createUser, createSession]);
-		}
+				user = await db.session.upsert({
+					where: {
+						userId_device: {
+							userId,
+							device
+						}
+					},
+					update: {
+						refreshTokenHash
+					},
+					create: {
+						userId,
+						device,
+						refreshTokenHash
+					}
+				});
+			} else {
+				userId = uuidv4();
 
-		return {
-			status: 200,
-			body: {
-				userId
-			},
-			headers: {
-				'Set-Cookie': [
-					cookie.serialize('accessToken', accessToken, {
-						path: '/',
-						httpOnly: true,
-						sameSite: 'strict',
-						secure: true,
-						maxAge: constants.ACCESS_TOKEN_EXPIRE_TIME
-					}),
-					cookie.serialize('refreshToken', refreshToken, {
-						path: '/',
-						httpOnly: true,
-						sameSite: 'strict',
-						secure: true,
-						maxAge: constants.REFRESH_TOKEN_EXPIRE_TIME
-					})
-				]
+				accessToken = await jwt.sign(
+					{ userId, exp: Math.floor(Date.now() / 1000) + constants.ACCESS_TOKEN_EXPIRE_TIME },
+					import.meta.env.VITE_ACCESS_TOKEN_SECRET
+				);
+				refreshToken = await jwt.sign(
+					{ userId, exp: Math.floor(Date.now() / 1000) + constants.REFRESH_TOKEN_EXPIRE_TIME },
+					import.meta.env.VITE_REFRESH_TOKEN_SECRET
+				);
+				refreshTokenHash = crypto.createHash('sha256').update(refreshToken, 'utf8').digest('hex');
+
+				const createUser = db.user.create({
+					data: {
+						id: userId,
+						stxAddress
+					}
+				});
+
+				const createSession = db.session.create({
+					data: {
+						userId,
+						refreshTokenHash,
+						device
+					}
+				});
+
+				user = await db.$transaction([createUser, createSession]);
 			}
-		};
+
+			return {
+				status: 200,
+				body: {
+					userId
+				},
+				headers: {
+					'Set-Cookie': [
+						cookie.serialize('accessToken', accessToken, {
+							path: '/',
+							httpOnly: true,
+							sameSite: 'strict',
+							secure: true,
+							maxAge: constants.ACCESS_TOKEN_EXPIRE_TIME
+						}),
+						cookie.serialize('refreshToken', refreshToken, {
+							path: '/',
+							httpOnly: true,
+							sameSite: 'strict',
+							secure: true,
+							maxAge: constants.REFRESH_TOKEN_EXPIRE_TIME
+						})
+					]
+				}
+			};
+		} catch (error) {
+			return {
+				status: 400,
+				body: {
+					error: error.message
+				}
+			};
+		}
 	} else {
 		return {
 			status: 400,
